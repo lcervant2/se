16ac7c50cd4d4d0431a7d37c98cd4cb90b7c2398
Luis N Cervantes <lcervant@hotmail.com>
2018-06-03 00:36:38 -0400

    new information

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# se
+# Secret Code Translator
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Key Features](#key-features)
+- [Tech Used](#tech-used)
+- [Architecture Diagram](#architecture-diagram)
+- [UI Mockup](#ui-mockup)
+
+## Getting Started
+## Architecture Diagram
+## UI Mockup
+<img src="2018-05-23_01-08-38.png" width="700" />
---
+++ scripts/logic.js
@@ -0,0 +1,10 @@
+// Create an array of letters
+var words_array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var words_values = [];
+var words-ids = [];
+var words-switch = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27"];
+// Creat an array of numbers
+var numbers_array = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27"];
+var numbers_values = [];
+var numbers-ids = [];
+var numbers-switch = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
